64edb814e7ed51abc736d997c9ff48253d98f142
Doni Rivaldo Simamora <donisimamora15@gmail.com>
2026-05-25 10:19:27 +0700

    Fix ./gradlew bootRun error

--- teknisio_backend/build.gradle.kts
+++ teknisio_backend/build.gradle.kts
@@ -32,22 +32,8 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
 
-	// Hotreload
+	// Hotreload (don't forget to delete this)
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-	// Validation
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-
-	// JWT Token
-	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-
-	// For migration db
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
-
-
 }
 
 tasks.withType<Test> {
