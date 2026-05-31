eef7d298b528702f94016bb623188b048bf2d4df
Doni Rivaldo Simamora <donisimamora15@gmail.com>
2026-05-31 22:08:46 +0700

    Okok

--- teknisio_backend/build.gradle.kts
+++ teknisio_backend/build.gradle.kts
@@ -10,7 +10,7 @@ description = "Backend service for Teknisio"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
